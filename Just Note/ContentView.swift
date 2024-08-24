a76f4da4b64b9171aa3ccea5f2ccfcceb4ce74b2
zhihaofans <zhihaofans@hotmail.com>
2024-08-25 00:04:30 +0800

    Add AppName, add views

--- Just Note/ContentView.swift
+++ Just Note/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        #if os(iOS)
+        MainView()
+        #else
+        Text("Hello Swift")
+        #endif
     }
 }
 
---
+++ Just Note/Views/MainView.swift
@@ -0,0 +1,46 @@
+//
+//  MainView.swift
+//  Just Note
+//
+//  Created by zzh on 2024/8/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @State private var selectedTab = 0
+    var body: some View {
+        switch selectedTab {
+        case 2:
+            SettingView()
+        default:
+            HomeView()
+        }
+        TabView(selection: $selectedTab) {
+            Text("")
+                .tabItem {
+                    Label("主页", systemImage: "house")
+                }
+                .tag(0)
+
+            Text("")
+                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
+                .tabItem {
+                    Label("动态", systemImage: "fanblades")
+                }
+                .tag(1)
+
+            Text("")
+                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
+                .tabItem {
+                    Label("更多", systemImage: "ellipsis")
+                }
+                .tag(2)
+        }
+        .frame(maxHeight: 50) // 限制最大高度
+    }
+}
+
+#Preview {
+    MainView()
+}
