808c7ceabb0aa9de73ee5cf255c54176302727aa
zhihaofans <zhihaofans@hotmail.com>
2024-08-24 23:53:52 +0800

    Initial Commit

---
+++ Just Note/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Just Note
+//
+//  Created by zzh on 2024/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Just Note/Just_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_NoteApp.swift
+//  Just Note
+//
+//  Created by zzh on 2024/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Just_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
