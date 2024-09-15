527365bec1177bfc2f9ecc3cabc9932fb7a1dc50
zhihaofans <zhihaofans@hotmail.com>
2024-09-16 00:15:52 +0800

    优化更新

--- Just Note/Views/HomeView.swift
+++ Just Note/Views/HomeView.swift
@@ -18,75 +18,110 @@ struct HomeView: View {
     var body: some View {
         NavigationStack(path: $noteList) {
             VStack {
-//                if false {
+                //                if false {
                 ////                    Text("点右上角记点东西").font(.largeTitle)
-//
-//                    NavigationLink(destination: EditView(editNoteItem: nil)) {
-//                        //                        Button("随便记一下") {
-//                        //                            // TODO: add note
-//                        //                        }
-//                        //                        .buttonStyle(.borderedProminent)
-//                        Text("随便记一下")
-//                            .padding()
-//                            .background(Color.blue)
-//                            .foregroundColor(.white)
-//                            .cornerRadius(8)
-//                    }
-//                TODO: 下面代码在实机ios 18 Beta失效（应该是ContentUnavailableView）
-//                    ContentUnavailableView {
-//                        Label("什么都没记", systemImage: "questionmark.folder.ar")
-//                    } actions: {
-//                        Spacer()
-//                        NavigationLink(destination: EditView(editNoteItem: nil)) {
-//                            //                        Button("随便记一下") {
-//                            //                            // TODO: add note
-//                            //                        }
-//                            //                        .buttonStyle(.borderedProminent)
-//                            Text("随便记一下")
-//                                .padding()
-//                                .background(Color.blue)
-//                                .foregroundColor(.white)
-//                                .cornerRadius(8)
-//                        }
-//                    }
-//                } else {
+                //
+                //                    NavigationLink(destination: EditView(editNoteItem: nil)) {
+                //                        //                        Button("随便记一下") {
+                //                        //                            // TODO: add note
+                //                        //                        }
+                //                        //                        .buttonStyle(.borderedProminent)
+                //                        Text("随便记一下")
+                //                            .padding()
+                //                            .background(Color.blue)
+                //                            .foregroundColor(.white)
+                //                            .cornerRadius(8)
+                //                    }
+                //                TODO: 下面代码在实机ios 18 Beta失效（应该是ContentUnavailableView）
+                //                    ContentUnavailableView {
+                //                        Label("什么都没记", systemImage: "questionmark.folder.ar")
+                //                    } actions: {
+                //                        Spacer()
+                //                        NavigationLink(destination: EditView(editNoteItem: nil)) {
+                //                            //                        Button("随便记一下") {
+                //                            //                            // TODO: add note
+                //                            //                        }
+                //                            //                        .buttonStyle(.borderedProminent)
+                //                            Text("随便记一下")
+                //                                .padding()
+                //                                .background(Color.blue)
+                //                                .foregroundColor(.white)
+                //                                .cornerRadius(8)
+                //                        }
+                //                    }
+                //                } else {
                 // 下面是老代码
-//                    List(noteList, id: \.id) { item in
-//                        NoteItemView(item: item)
-//                            .swipeActions {
-//                                Button {
-//                                    // TODO: 打开链接
-//                                    // deleteItem(item: item)
-//                                    openLink(link: item.url)
-//                                } label: {
-//                                    Text("打开链接") // 自定义删除文本
-//                                }
-//                                .tint(.green) // 自定义删除按钮颜色
-//                                //
-//                                //                        Button {
-//                                //                            print("Pinned \(item)")
-//                                //                        } label: {
-//                                //                            Text("Pin") // 自定义"Pin"操作
-//                                //                        }
-//                                //                        .tint(.blue) // 自定义操作颜色
-//                            }
-//                    }
+                //                    List(noteList, id: \.id) { item in
+                //                        NoteItemView(item: item)
+                //                            .swipeActions {
+                //                                Button {
+                //                                    // TODO: 打开链接
+                //                                    // deleteItem(item: item)
+                //                                    openLink(link: item.url)
+                //                                } label: {
+                //                                    Text("打开链接") // 自定义删除文本
+                //                                }
+                //                                .tint(.green) // 自定义删除按钮颜色
+                //                                //
+                //                                //                        Button {
+                //                                //                            print("Pinned \(item)")
+                //                                //                        } label: {
+                //                                //                            Text("Pin") // 自定义"Pin"操作
+                //                                //                        }
+                //                                //                        .tint(.blue) // 自定义操作颜色
+                //                            }
+                //                    }
 
                 // 下面是新代码SwiftData
                 // 显示所有任务
-
-                List(notes) { task in
-                    HStack {
-                        Text(task.text)
-                        Spacer()
-                        Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
-//                                .onTapGesture {
-//                                    toggleTaskCompletion(task)
-//                                }
+                if notes.isEmpty {
+                    NavigationLink(destination: EditView(path: noteList)) {
+                        //                        Button("随便记一下") {
+                        //                            // TODO: add note
+                        //                        }
+                        //                        .buttonStyle(.borderedProminent)
+                        Text("随便记一下")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
                     }
-                }.onAppear {
-                    print(notes.length)
+                } else {
+                    List(notes) { item in
+                        NoteItemView(path: noteList, item: item)
+                            .swipeActions {
+                                if item.type.lowercased() == "url" && item.url.isNotEmpty {
+                                    Button {
+                                        // TODO: 打开链接
+                                        // deleteItem(item: item)
+                                        openLink(link: item.url)
+                                    } label: {
+                                        Text("打开链接") // 自定义删除文本
+                                    }
+                                    .tint(.green) // 自定义删除按钮颜色
+                                }
+                                //
+                                //                        Button {
+                                //                            print("Pinned \(item)")
+                                //                        } label: {
+                                //                            Text("Pin") // 自定义"Pin"操作
+                                //                        }
+                                //                        .tint(.blue) // 自定义操作颜色
+                            }
+                    }
                 }
+//                List(notes) { task in
+//                    HStack {
+//                        Text(task.text)
+//                        Spacer()
+//                        Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+                ////                                .onTapGesture {
+                ////                                    toggleTaskCompletion(task)
+                ////                                }
+//                    }
+//                }.onAppear {
+//                    print(notes.length)
+//                }
 //                    .onDelete(perform: deletedTodoItem)
 //                }
             }
