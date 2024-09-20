ca1ad8d40cd60689fa618c5a1f05877f969f9735
zhihaofans <zhihaofans@hotmail.com>
2024-09-20 23:23:37 +0800

    新增测试功能 分词

--- Just Note/Views/LabView.swift
+++ Just Note/Views/LabView.swift
@@ -5,6 +5,7 @@
 //  Created by zzh on 2024/9/7.
 //
 
+import NaturalLanguage
 import SwiftUI
 
 struct LabView: View {
@@ -18,6 +19,9 @@ struct LabView: View {
                         NavigationLink(destination: TagView()) {
                             Text("标签")
                         }
+                        NavigationLink(destination: FenciView()) {
+                            Text("文本分词")
+                        }
                     }
                 }
             }
@@ -26,6 +30,54 @@ struct LabView: View {
                 .navigationBarTitleDisplayMode(.inline) // 标题保持较小尺寸，始终在导航栏中显示
             #endif
         }
+    }
+}
+
+struct FenciView: View {
+    @State private var inputText: String
+    @State private var textList: [String]
+    @State private var fenciUnit: NLTokenUnit
+    @State private var fenciUnitTitle: String
+    init(inputText: String = "") {
+        self.inputText = inputText
+        self.textList = []
+        self.fenciUnit = .word
+        self.fenciUnitTitle = "按单词分词"
+    }
+
+    var body: some View {
+        Menu {
+            Button(action: {
+                fenciUnit = .word
+                fenciUnitTitle = "按单词分词"
+            }) {
+                Label("按单词分词", systemImage: "character")
+            }
+        } label: {
+            SimpleTextItemView(title: "分词类型", detail: fenciUnitTitle)
+        }
+        TextField("Placeholder", text: $inputText)
+            .onChange(of: inputText) {
+                textList = fenci(inputText)
+            }
+        List(textList, id: \.self) {
+            Text($0)
+        }
+    }
+
+    private func fenci(_ text: String) -> [String] {
+        let tokenizer = NLTokenizer(unit: fenciUnit)
+        tokenizer.string = text
+
+        var tokens: [String] = []
+
+        tokenizer.enumerateTokens(in: text.startIndex ..< text.endIndex) { tokenRange, _ in
+            let token = String(text[tokenRange])
+            tokens.append(token)
+            return true
+        }
+
+        return tokens
     }
 }
 
