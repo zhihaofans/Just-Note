1f857b5627b29a592eaeb693c99247da8ccebad7
zhihaofans <zhihaofans@hotmail.com>
2024-09-25 08:54:00 +0800

    image类型的界面

--- Just Note/Views/Componets/ListItemView.swift
+++ Just Note/Views/Componets/ListItemView.swift
@@ -64,7 +64,37 @@ struct TextTagItemView: View {
         }
     }
 }
+struct TextTagImageItemView: View {
+    var text: String
+    var tags: [String]
+    var image_data: Data
 
+    var body: some View {
+        HStack {
+            VStack {
+                Text(text)
+                    .lineLimit(1)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    Text(tags.compactMap { "#" + $0 }.joined(separator: " , "))
+                        .lineLimit(1)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.gray)
+                }
+                Image(data: image_data)
+                    .resizable() // 允许图片可调整大小
+                    .scaledToFit() // 图片将等比缩放以适应框架
+                    .frame(width: 120, height: 120) // 设置视图框架的大小
+                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous)) // 设置圆角矩形形状
+                    .shadow(radius: 5) // 添加阴影以增强效果
+            }
+            Spacer()
+            // Text(text).foregroundColor(.gray)
+        }
+    }
+}
+
+// 下面是设置界面用
 struct iconAndTextItemView: View {
     var title: String
     var icon: String
