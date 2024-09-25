1de43481d04995a9c63af393fa4939a3103c28ee
zhihaofans <zhihaofans@hotmail.com>
2024-09-25 08:54:10 +0800

    优化代码

--- Just Note/Views/HomeView.swift
+++ Just Note/Views/HomeView.swift
@@ -18,60 +18,6 @@ struct HomeView: View {
     var body: some View {
         NavigationStack(path: $noteList) {
             VStack {
-                //                if false {
-                ////                    Text("点右上角记点东西").font(.largeTitle)
-                //
-                //                    NavigationLink(destination: EditView(editNoteItem: nil)) {
-                //                        //                        Button("随便记一下") {
-                //                        //                            // TODO: add note
-                //                        //                        }
-                //                        //                        .buttonStyle(.borderedProminent)
-                //                        Text("随便记一下")
-                //                            .padding()
-                //                            .background(Color.blue)
-                //                            .foregroundColor(.white)
-                //                            .cornerRadius(8)
-                //                    }
-                //                TODO: 下面代码在实机ios 18 Beta失效（应该是ContentUnavailableView）
-                //                    ContentUnavailableView {
-                //                        Label("什么都没记", systemImage: "questionmark.folder.ar")
-                //                    } actions: {
-                //                        Spacer()
-                //                        NavigationLink(destination: EditView(editNoteItem: nil)) {
-                //                            //                        Button("随便记一下") {
-                //                            //                            // TODO: add note
-                //                            //                        }
-                //                            //                        .buttonStyle(.borderedProminent)
-                //                            Text("随便记一下")
-                //                                .padding()
-                //                                .background(Color.blue)
-                //                                .foregroundColor(.white)
-                //                                .cornerRadius(8)
-                //                        }
-                //                    }
-                //                } else {
-                // 下面是老代码
-                //                    List(noteList, id: \.id) { item in
-                //                        NoteItemView(item: item)
-                //                            .swipeActions {
-                //                                Button {
-                //                                    // TODO: 打开链接
-                //                                    // deleteItem(item: item)
-                //                                    openLink(link: item.url)
-                //                                } label: {
-                //                                    Text("打开链接") // 自定义删除文本
-                //                                }
-                //                                .tint(.green) // 自定义删除按钮颜色
-                //                                //
-                //                                //                        Button {
-                //                                //                            print("Pinned \(item)")
-                //                                //                        } label: {
-                //                                //                            Text("Pin") // 自定义"Pin"操作
-                //                                //                        }
-                //                                //                        .tint(.blue) // 自定义操作颜色
-                //                            }
-                //                    }
-
                 // 下面是新代码SwiftData
                 // 显示所有任务
                 if notes.isEmpty {
